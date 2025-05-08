ecf19a646136d6e706929124d987576f912c9856
documentation-action <stc-support@dnv.com>
2025-05-08 06:37:07 +0000

    ASVA

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((azure-team . "STC Support")
-	 (azure-project . "Open Simulation Platform")
-	 (azure-organization . "GTR-DigitalAssurance")
-	 (cider-default-cljs-repl . shadow)
-	 (cider-preferred-build-tool . shadow-cljs)
-	 (cider-shadow-default-options . ":app"))))
